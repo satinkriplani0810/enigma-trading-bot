4c0bd3f515bef5426e4cb60ae040c8a36427e2a1
nirupam <nirupamsingh29@gmail.com>
2017-07-20 19:45:17 +0530

    added requirements.txt and reformatted EnigmaTradingBot

--- EnigmaTradingBot.py
+++ EnigmaTradingBot.py
@@ -1,12 +1,10 @@
 import configparser
-import json
 import logging
-from poloniex import poloniex
-from Bittrex import Bittrex
 
-from urllib.request import urlopen
 from telegram.ext import CommandHandler
 from telegram.ext import Updater
+
+from Bittrex import Bittrex
 
 
 class EnigmaTradingBot:
@@ -26,41 +24,40 @@ class EnigmaTradingBot:
         self.generate_handler('ticker')
         self.updater.start_polling()
 
-    def start(self, bot, update, args):
+    @staticmethod
+    def start(bot, update, args):
         reply = "*[Available Commands]*\n\n Get currency ticker:\n /ticker BTC ETH\n"
-        bot.send_message(chat_id=update.message.chat_id,
-                        text=reply,
-                        parse_mode="Markdown")
+        bot.send_message(chat_id=update.message.chat_id, text=reply, parse_mode="Markdown")
         logging.info('start')
 
-    def ticker(self, bot, update, args):
+    @staticmethod
+    def ticker(bot, update, args):
         bittrex = Bittrex("xyz", "xyz")
 
-        #Generating the market pair from the user input arguments
+        # Generating the market pair from the user input arguments
         selected_pair = args[0].upper() + "-" + args[1].upper()
         currency = args[0].upper()
+        formatted_reply = ""
 
-        #Bittrex
+        # Bittrex
         try:
             response = bittrex.get_ticker(selected_pair)
             logging.debug("Response: " + str(response))
 
-            if(response['success'] == True):
+            if response['success']:
                 formatted_reply = "*[" + selected_pair + "]*\n"
                 formatted_reply += "Current: " + str(response['result']['Last']) + " " + currency +\
                     "\nAsk: " + str(response['result']['Ask']) + " " + currency +\
                     "\nBid: " + str(response['result']['Bid']) + " " + currency
             else:
-                formatted_reply = "Error: "  + str(response['message']).replace('_', ' ')
+                formatted_reply = "Error: " + str(response['message']).replace('_', ' ')
 
         except Exception as e:
             formatted_reply = "Error in invoking Bittrex API."           
             logging.error(e)
 
         finally: 
-            bot.send_message(chat_id=update.message.chat_id,
-                text=formatted_reply,
-                parse_mode="Markdown")
+            bot.send_message(chat_id=update.message.chat_id, text=formatted_reply, parse_mode="Markdown")
 
 
 if __name__ == '__main__':
